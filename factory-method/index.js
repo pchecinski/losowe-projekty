e2fd4f8cb492c771dcec0c923d21f3aebbc9e1d3
Patryk Chęciński <patryk@checinski.dev>
2020-03-24 19:11:03 +0100

    Add files via upload

---
+++ factory-method/index.js
@@ -0,0 +1,64 @@
+class Factory {
+    createEmployee = (type) => {
+        let employee;
+ 
+        if (type === "fulltime") {
+            employee = new FullTime();
+        } else if (type === "parttime") {
+            employee = new PartTime();
+        } else if (type === "temporary") {
+            employee = new Temporary();
+        } else if (type === "contractor") {
+            employee = new Contractor();
+        }
+ 
+        employee.type = type;
+ 
+        employee.log = function () {
+            console.log(`Pracownik ${this.type} zarobki ${this.hourly}`);
+        }
+ 
+        return employee;
+    }
+}
+ 
+class FullTime {
+    constructor() {
+        this.hourly = "$12";
+    }
+};
+ 
+class PartTime {
+    constructor() {
+        this.hourly = "$11";
+    }
+};
+ 
+class Temporary {
+    constructor() {
+        this.hourly = "$10";
+    }
+};
+ 
+class Contractor {
+    constructor() {
+        this.hourly = "$15";
+    }
+};
+ 
+ 
+function run() {
+    var employees = [];
+    var factory = new Factory();
+ 
+    employees.push(factory.createEmployee("fulltime"));
+    employees.push(factory.createEmployee("parttime"));
+    employees.push(factory.createEmployee("temporary"));
+    employees.push(factory.createEmployee("contractor"));
+    
+    for (var i = 0, len = employees.length; i < len; i++) {
+        employees[i].log();
+    }
+}
+
+run();
